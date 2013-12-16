b0f4814a6d007c82577254977c4bca5559a70989
Simon Weber <weber@tu-berlin.de>
2013-12-16 11:31:15 +0000

    Simulation: von Mises with smaller learning rate

--- experiment_using_snep.py
+++ experiment_using_snep.py
@@ -30,8 +30,8 @@ def main():
 	tables = exp.tables
 
 	target_rate = 5.0
-	n_exc = 100
-	n_inh = 100
+	n_exc = 10000
+	n_inh = 10000
 	radius = 0.5
 	init_weight_exc = 20. * target_rate / n_exc
 	init_weight_inh = 5. * target_rate / n_inh
@@ -60,8 +60,9 @@ def main():
 			},
 		'inh': 
 			{
-			# 'sigma_x':ParameterArray([1.5, 0.2, 0.04, 0.2, 0.15, 0.15]),
-			# 'sigma_y':ParameterArray([0.04, 0.04, 1.5, 1.5, 0.04, 1.5]),
+			'sigma_x':ParameterArray([1.5, 0.2, 0.04, 0.2, 0.15, 0.15]),
+			'sigma_y':ParameterArray([0.04, 0.04, 1.5, 1.5, 0.04, 1.5]),
+			'eta':ParameterArray([1e-6, 1e-7]),
 			# 'n':ParameterArray([1, 100]),
 			# 'fields_per_synapse':ParameterArray([1, 4, 8]),
 			# 'weight_overlap':ParameterArray([0.0, 0.2]),
@@ -73,59 +74,60 @@ def main():
 			},
 		'sim': 
 			{
-			'weight_lateral':ParameterArray(
-				[1.0, 2.0, 4.0, 8.0]),
-			'output_neurons':ParameterArray([1, 2, 4, 8]),
+			# 'weight_lateral':ParameterArray(
+			# 	[1.0, 2.0, 4.0, 8.0]),
+			# 'output_neurons':ParameterArray([1, 2, 4, 8]),
 			# 'seed_trajectory':ParameterArray([1, 2]),
 			# 'initial_y':ParameterArray([-0.2, 0.2]),
 			# 'seed_init_weights':ParameterArray([3, 4]),
 			'seed_centers':ParameterArray([2, 3]),
-			'dt':ParameterArray([0.01])
+			# 'dt':ParameterArray([0.01])
 			# 'boxtype':ParameterArray(boxtype),
 			},
 
 	}
 	
 	params = {
-		'visual': 'figure', 
+		'visual': 'video', 
 		'sim':
 			{
-			'lateral_inhibition': True,
-			'output_neurons': 2,
+			'lateral_inhibition': False,
+			'output_neurons': 1,
 			'weight_lateral': 0.1,
 			'tau': 0.2,
 			'symmetric_centers': False,
-			'dimensions': 1,
+			'dimensions': 2,
 			'boxtype': 'linear',
 			'radius': radius,
 			'diff_const': 0.01,
-			'every_nth_step': 100,
-			'every_nth_step_weights': 1e5,
+			'every_nth_step': 1,
+			'every_nth_step_weights': 20000,
 			'seed_trajectory': 3,
 			'seed_init_weights': 3,
 			'seed_centers': 3,
-			'simulation_time': 1e5,
-			'dt': 0.01,
+			'simulation_time': 1e7,
+			'dt': 1.0,
 			'initial_x': 0.1,
 			'initial_y': 0.2,
 			'velocity': 0.01,
 			'persistence_length': 0.5,
-			'motion': 'diffusive',
+			'motion': 'persistent_semiperiodic',
 			# 'motion': 'diffusive',
-			'boundary_conditions': 'reflective',	
+			'boundary_conditions': 'billiard',	
 			},
 		'out':
 			{
 			'target_rate': target_rate,
-			'normalization': 'quadratic_multiplicative_lateral_inhibition'
+			'normalization': 'quadratic_multiplicative'
+			# 'normalization': 'quadratic_multiplicative_lateral_inhibition'
 			},
 		'exc':
 			{
 			'weight_overlap': 0.0,
-			'eta': 1e-6,
-			'sigma': 0.03,
-			'sigma_spreading': 1e-6,
-			'sigma_distribution': 'cut_off_gaussian_with_standard_limits',
+			'eta': 1e-9,
+			'sigma': 0.05,
+			'sigma_spreading': 0.0,
+			'sigma_distribution': 'uniform',
 			'sigma_x': 0.05,
 			'sigma_y': 0.05,
 			'n': n_exc,
@@ -137,13 +139,13 @@ def main():
 		'inh':
 			{
 			'weight_overlap': 0.0,
-			'eta': 1e-4,
-			'sigma': 0.1,
+			'eta': 1e-6,
+			'sigma': 0.15,
 			# 'sigma_spreading': {'stdev': 0.01, 'left': 0.01, 'right': 0.199},
-			'sigma_spreading': 1e-6,
-			'sigma_distribution': 'cut_off_gaussian_with_standard_limits',
-			'sigma_x': 0.15,
-			'sigma_y': 0.15,
+			'sigma_spreading': 0.0,
+			'sigma_distribution': 'uniform',
+			'sigma_x': 0.1,
+			'sigma_y': 0.2,
 			'n': n_inh,
 			'fields_per_synapse': 1,
 			'init_weight':init_weight_inh,
@@ -156,10 +158,10 @@ def main():
 	tables.add_parameters(params)
 
 	# Note: maybe change population to empty string
-	# linked_params_tuples_1 = [
-	#	('inh', 'sigma_x'),
-	#	('inh', 'sigma_y')]
-	# tables.link_parameter_ranges(linked_params_tuples_1)
+	linked_params_tuples_1 = [
+		('inh', 'sigma_x'),
+		('inh', 'sigma_y')]
+	tables.link_parameter_ranges(linked_params_tuples_1)
 
 	# linked_params_tuples_1 = [
 	# 	('inh', 'fields_per_synapse'),
