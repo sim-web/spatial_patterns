377515280477596352514aec4cad9945118fcb41
Simon Weber <weber@tu-berlin.de>
2014-01-09 18:39:32 +0000

    Type commit message here…

--- experiment_using_snep.py
+++ experiment_using_snep.py
@@ -57,7 +57,7 @@ def main():
 			# 'weight_overlap':ParameterArray([0.0, 0.2]),
 			# 'sigma_x':ParameterArray([0.05, 0.1, 0.2]),
 			# 'sigma_y':ParameterArray([0.05]),
-			# 'eta':ParameterArray([1e-6, ]),
+			'eta':ParameterArray([1e-6, 1e-5, 1e-4]),
 			# 'sigma':ParameterArray([0.03, 0.05]),
 			# 'sigma_spreading':ParameterArray([1e-4, 1e-3, 1e-2, 1e-1]),
 			# 'init_weight_noise':ParameterArray(init_weight_noise),
@@ -67,7 +67,7 @@ def main():
 			{
 			# 'sigma_x':ParameterArray([1.5, 0.2, 0.04, 0.2, 0.15, 0.15]),
 			# 'sigma_y':ParameterArray([0.04, 0.04, 1.5, 1.5, 0.04, 1.5]),
-			# 'eta':ParameterArray([1e-3]),
+			'eta':ParameterArray([1e-4, 1e-3, 1e-2]),
 			'init_weight':ParameterArray([0.25, 1.0]),
 			# 'n':ParameterArray([100, 80, 60, 40, 20]),
 			# 'fields_per_synapse':ParameterArray([1, 4, 8]),
@@ -80,7 +80,7 @@ def main():
 			},
 		'sim': 
 			{
-			'seed_centers':ParameterArray([1, 2, 3, 4, 5, 6, 7, 8, 9]),
+			'seed_centers':ParameterArray([3, 4, 5]),
 			'gaussians_with_height_one':ParameterArray([False, True]),
 			# 'weight_lateral':ParameterArray(
 				# [0.0, 0.5, 0.9, 1.0, 1.1, 2.0, 4.0, 8.0, 16.0]),
@@ -133,7 +133,7 @@ def main():
 			'persistence_length': 0.5,
 			# 'motion': 'persistent_semiperiodic',
 			'motion': 'diffusive',
-			# 'boundary_conditions': 'reflective',
+			# 'boundary_conditions': 'reflective',n
 			},
 		'out':
 			{
