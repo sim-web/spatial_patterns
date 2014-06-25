3e24200141453dd8940ce2f615c05dbcda2cad2c
Simon Weber <weber@tu-berlin.de>
2014-06-25 15:59:01 +0000

    Simulation: Conjunctive Cells and some grid cells

--- experiment_using_snep.py
+++ experiment_using_snep.py
@@ -88,14 +88,16 @@ def main():
 
 	sigma_exc = np.array([
 						[0.05, 0.05],
-						[0.05, 0.05],
-						[0.05, 0.07]
+						[0.07, 0.05],
+						[0.07, 0.07],
+						[0.03, 0.03],
 						])
 
 	sigma_inh = np.array([
-						[0.15, 0.15],
-						[0.2, 0.04],
-						[0.2, 0.04]
+						[0.15, 1.50],
+						[0.15, 1.50],
+						[0.15, 1.50],
+						[0.10, 0.10],
 						])
 
 	weight_overlap_exc = 3*sigma_exc
@@ -180,7 +182,7 @@ def main():
 			{
 			'input_space_resolution':ParameterArray(np.amin(sigma_exc, axis=1) / 10.),
 			# 'symmetric_centers':ParameterArray([False, True]),
-			'seed_centers':ParameterArray([1, 2]),
+			'seed_centers':ParameterArray([1, 2, 3]),
 			# 'radius':ParameterArray(radius),
 			# 'gaussians_with_height_one':ParameterArray([False, True]),
 			# 'weight_lateral':ParameterArray(
