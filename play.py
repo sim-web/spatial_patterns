b0c68dcdbca245f93d221bca1558df75809de6c7
Simon Weber <weber@tu-berlin.de>
2014-07-15 17:12:06 +0000

    New get_equidistant_positions function, which works in higher dimensions and is way shorter.

--- play.py
+++ play.py
@@ -2,75 +2,75 @@ import numpy as np
 import matplotlib as mpl
 mpl.use('TkAgg')
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-def get_equidistant_positions(r, n_x, n_y, boxtype='linear', distortion=0.):
-	"""Returns equidistant, symmetrically distributed 2D coordinates
+
+def get_equidistant_positions(r, n, boxtype='linear', distortion=0.):
+	"""Returns equidistant, symmetrically distributed coordinates
 	
-	Note: The number of returned positions will be <= n, because not
-		because only numbers where n = k**2 where k in Integers, can
-		exactly tile the space and only for 'linear' arrangements anyway.
-		In the case of circular boxtype many positions need to be thrown
-		away.
+	The coordinates are taken such that they don't lie on the boundaries
+	of the environment but instead half a lattice constant away on each
+	side.
+	Note: In the case of circular boxtype positions outside the cirlce
+		are thrown away.
 
 	Parameters
 	----------
 	r : ndarray
 		Dimensions of the box
-	n_x, n_y: int, int
-		Number of centers along x and y axis
+	n : ndarray
+		Array of same shape as r, number of positions along each direction
 	boxtype : string
 		'linear': A quadratic arrangement of positions is returned
 		'circular': A ciruclar arrangement instead 
 	distortion : float or ndarray
 		Maximal length by which each lattice coordinate (x and y separately)
 		is shifted randomly (uniformly)
-	
 	Returns
 	-------
-	(ndarray) of shape (m, 2), where m < n but close to n for linear boxtype
-				and signficantly smaller than n for circular boxtype
+	(ndarray) of shape (m, len(n)), where m < np.prod(n) for boxtype
+	'circular', because points at the edges are thrown away.
 	"""
-	dx = 2*r[0]/(2*n_x)
-	dy = 2*r[1]/(2*n_y)
-	x_space = np.linspace(-r[0]+dx, r[0]-dx, n_x)
-	y_space = np.linspace(-r[1]+dy, r[1]-dy, n_y)
-	positions_grid = np.empty((n_x, n_y, 2))
-	X, Y = np.meshgrid(x_space, y_space)
-	# Put all the positions in positions_grid
-	for n_y, y in enumerate(y_space):
-		for n_x, x in enumerate(x_space):
-			positions_grid[n_x][n_y] =  [x, y]
-	# Flatten for easier reshape
-	positions = positions_grid.flatten()
-	# Reshape to the desired shape
-	positions = positions.reshape(positions.size/2, 2)
+	dimensions = len(n)
+	d = 2*r/(2*n)
+	spaces = [np.linspace(-ra+da, ra-da, na) for (ra, na, da) in zip(r, n, d)]
+	Xs = np.meshgrid(*spaces)
 	if boxtype == 'circular':
-		r = np.amax(r)
-		distance = np.sqrt(X*X + Y*Y)
-		# Set all position values outside the circle to NaN
-		positions_grid[distance.T>r] = np.nan
-		positions = positions_grid.flatten()
-		isnan = np.isnan(positions)
-		# Delete all positions outside the circle
-		positions = np.delete(positions, np.nonzero(isnan))
-		# Bring into desired shape
-		positions = positions.reshape(positions.size/2, 2)
+		distance = np.sqrt(np.sum([x**2 for x in Xs], axis=0))
+		for x in Xs:
+			x[distance>r[0]] = np.nan
+	positions = np.array(zip(*[x.flat for x in Xs]))
+	positions = positions.flatten()
+	isnan = np.isnan(positions)
+	np.delete(positions, np.nonzero(isnan))
+	positions = positions.reshape(positions.size/dimensions, dimensions)
 	distortion_array = 2*distortion * np.random.random_sample(positions.shape) - distortion
 	return positions + distortion_array
 
 
-n_x = 70
-n_y = 70
-r = np.array([0.5, 0.5])
-boxtype = 'linear'
-distortion = r/np.array([n_x, n_y])
-centers = get_equidistant_positions(r=r, n_x=n_x, n_y=n_y, boxtype=boxtype, distortion=distortion)
-ax = plt.gca()
-plt.scatter(centers[...,0], centers[...,1], marker='x')
+n_x = 20
+n_y = 10
+n_z = 6
+
+n = np.array([n_x, n_y, n_z])
+r = np.array([0.5, 0.5, 0.5])
+# r = np.array([0.5, 0.5])
+boxtype = 'linear' 
+distortion = r/n
+# distortion = 0.0
+centers = get_equidistant_positions(r=r, n=n, boxtype=boxtype, distortion=distortion)
+fig = plt.figure()
+
+if len(n) == 2:
+	ax = fig.add_subplot(111)
+	ax.scatter(centers[...,0], centers[...,1], marker='x')
+elif len(n) == 3:
+	ax = fig.add_subplot(111, projection='3d')
+	ax.scatter(centers[...,0], centers[...,1], centers[...,2], marker='x')
+
 ax.add_artist(plt.Circle((0,0), r[0], fc='none'))
 ax.add_artist(plt.Rectangle((-r[0],-r[0]), 2*r[0], 2*r[0], fc='none'))
 ax.set_aspect('equal')
-print 'bla'
 plt.show()
 
 # def plot_polar(radius, spacing, a):
