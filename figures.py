ef0cf2eab3da7be166ac7f962b398c953f58b270
Simon Weber <weber@tu-berlin.de>
2014-07-30 13:42:54 +0000

    Type commit message here…

--- figures.py
+++ figures.py
@@ -11,6 +11,8 @@ import numpy as np
 import string
 import scipy.stats
 import os
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+
 os.environ['PATH'] = os.environ['PATH'] + ':/usr/texbin'
 
 ##############################################
@@ -27,6 +29,8 @@ mpl.rcParams.update({'figure.autolayout': True})
 colors = {'exc': '#D7191C', 'inh': '#2C7BB6', 'diff': '0.4'}
 legend = {'exc': 'Excitation', 'inh': 'Inhibition', 'diff': 'Difference'}
 legend_short = {'exc': 'Exc.', 'inh': 'Inh.', 'diff': 'Difference'}
+signs = {'exc': 1, 'inh': -1}
+sigma = {'exc': 0.005, 'inh': 0.005}
 marker = {'exc': '^', 'inh': 'o'}
 mpl.rcParams['legend.handlelength'] = 0
 
@@ -34,87 +38,117 @@ mpl.rcParams['legend.handlelength'] = 0
 ##########	Inhibitory Plasticity	##########
 ##############################################
 np.random.seed(20)
-fig = plt.figure(figsize=(4.4, 6.6))
-gs = gridspec.GridSpec(3, 1, height_ratios=[3,1,3]) 
+fig = plt.figure(figsize=(4.4, 7.6))
+gs = gridspec.GridSpec(3, 1, height_ratios=[3,3,1]) 
+populations = ['exc', 'inh']
+target_rate_distance = 0.55
+target_rate = 1.0
+
 
 plt.axis('off')
 plt.xticks([])
-# plt.xticks(inputs, np.arange(1, 10))
 plt.yticks([])
-# fig.patch.set_visible(False)
-# plt.axis('off')
-# plt.xlim([-0.02, 1.02])
-# plt.ylim([-2.2, 2.2])
-# plt.margins(0.025, 0.06)
+
 n = 12
 current = {} 
-# current['exc'] = np.array([0.1, 0.3, 0.75, 0.85, 0.9, 0.75, 0.4, 0.2, 0.1])
-# current['exc'] = np.array([0.1, 0.55, 0.75, 0.25, 0., 0.8, 0.7, 0.2, 0.1])
+general_settings = {'xticks': [], 'yticks': [], 'xmargin': 0.025,
+								'ymargin': 0.23}
+membrane_current_settings = {'ylabel': 'Membrane current'}
+membrane_current_settings.update(general_settings)
+### Before Learning ###
+plt.subplot(gs[0], title='Before learning', **membrane_current_settings)
+legend_loc = 'lower left'
+
 # shift = np.pi/2.3
 shift = 0.
 x = np.linspace(shift, 5*np.pi+shift, n)
 current['exc'] = np.sin(x) + 1.0*(2*np.random.random_sample(n) - 1)
 current['inh'] = np.zeros_like(current['exc'])
-# current['inh'] = current['exc'] + (0.00*np.random.random_sample(current['exc'].shape) - 0.25)
 inputs = np.linspace(0., 1., len(current['exc']))
 
-# fig.add_subplot(311, xticks=[], yticks=[], title='Before learning',
-# 					xlabel='Stimulus', ylabel='Membrane current',
-# 					xmargin=0.025, ymargin=0.06)
-
-membrane_current_settings = {'xticks': [], 'yticks': [], 'xmargin': 0.025,
-								'ymargin': 0.06}
-
-plt.subplot(gs[0], **membrane_current_settings)
-
-for p in ['exc', 'inh']:
+for p in populations:
 	label = legend_short[p]
 	plt.plot(inputs, current[p], marker=marker[p], color=colors[p], lw=1, markeredgewidth=0.0, label=label, markersize=10)
 
-
 ax = plt.gca()
-# ax.autoscale_view()
-plt.legend(loc='best', numpoints=1).draw_frame(False)
+ylim_before = ax.get_ylim()
+plt.legend(loc=legend_loc, numpoints=1).draw_frame(False)
 width = 0.002
 distances = current['exc'] - current['inh']
-print distances
+firing_rates = target_rate/target_rate_distance * distances
+firing_rates[firing_rates<0] = 0.
+print firing_rates
 for n, i in enumerate(inputs):
 	sign = np.sign(distances[n])
 	if abs(distances[n]) < 0.25:
 		arrow_length = 0.
 	else:
 		arrow_length = distances[n] - sign*0.25
-	print inputs[n]
 	plt.arrow(inputs[n], 0, 0, arrow_length, color=colors['inh'], transform=ax.transData, width=width, head_width=8*width, head_length=30*width)
 
-plt.subplot(gs[1], **membrane_current_settings)
+### Inset 1 ###
+axins = inset_axes(ax,
+                   width="30%",
+                   height="30%",
+                   loc=1,
+                   )
 
-# fig.add_subplot(312, xticks=[], yticks=[],	ylabel='Tuning',
-# 					xmargin=0.025, ymargin=0.06)
-plt.plot(np.arange(40), np.arange(40))
+plt.xticks([])
+# plt.yticks([0], fontsize=12)
+ax = plt.gca()
+ax.set_yticks((0, target_rate))
+ax.set_yticklabels((r'$0$', r'$\rho_0$'), fontsize=12)
+ax.text(-0.15, 0.9, r'$v$', transform = ax.transAxes, fontsize=12)
+plt.hlines(0., 0, 1)
+plt.hlines(target_rate, 0, 1, lw=2, linestyle='dotted', label=r'$\rho_0$')
+# plt.legend(fontsize=12).draw_frame(False)
+plt.margins(0., 0.2)
+plt.plot(inputs, firing_rates, color='black', lw=2)
+ylim_inset1 = ax.get_ylim()
 
-plt.subplot(gs[2], **membrane_current_settings)
 
-# fig.add_subplot(313, xticks=[], yticks=[], title='After learning',
-# 					xlabel='Stimulus', ylabel='Membrane current',
-# 					xmargin=0.025, ymargin=0.06)
+### Tuning ###
+plt.subplot(gs[2], frameon=False, **general_settings)
 
-current['inh'] = current['exc'] + (0.00*np.random.random_sample(current['exc'].shape) - 0.25)
-for p in ['exc', 'inh']:
+# plt.plot(np.arange(40), np.arange(40))
+for p in populations:
+	for n, i in enumerate(inputs):
+		gauss = scipy.stats.norm(loc=inputs[n], scale=sigma[p]).pdf
+		x = np.linspace(0, 1, 1001)
+		plt.plot(x, signs[p] * np.sqrt(2*np.pi*sigma[p]**2) * gauss(x),
+					color=colors[p])
+plt.ylabel('Tuning')
+
+### After Learning ###
+plt.subplot(gs[1], title='After learning', **membrane_current_settings)
+plt.ylim(*ylim_before)
+current['inh'] = current['exc'] + ((0.04*np.random.random_sample(current['exc'].shape) - 0.02) - target_rate_distance)
+distances = current['exc'] - current['inh']
+firing_rates = target_rate/target_rate_distance * distances
+firing_rates[firing_rates<0] = 0.
+for p in populations:
 	label = legend_short[p]
 	plt.plot(inputs, current[p], marker=marker[p], color=colors[p], lw=1, markeredgewidth=0.0, label=label, markersize=10)
+plt.legend(loc=legend_loc, numpoints=1).draw_frame(False)
 
+### Inset 2 ###
+ax = plt.gca()
+axins = inset_axes(ax,
+                   width="30%",
+                   height="30%",
+                   loc=1,
+                   )
 
-# plt.arrow(inputs[1], 0.2, 0, 0.3 , color=colors['inh'], transform=ax.transData, width=width, head_width=8*width, head_length=30*width)
-# plt.arrow(inputs[6], -0.2, 0, -0.3 , color=colors['inh'], transform=ax.transData, width=width, head_width=8*width, head_length=30*width)
-# plt.arrow(inputs[10], 0.2, 0, 0.3 , color=colors['inh'], transform=ax.transData, width=width, head_width=8*width, head_length=30*width)
+plt.xticks([])
+# plt.yticks([0], fontsize=12)
+ax = plt.gca()
+ax.set_yticks((0, target_rate))
+ax.set_yticklabels((r'$0$', r'$\rho_0$'), fontsize=12)
+ax.text(-0.15, 0.9, r'$v$', transform = ax.transAxes, fontsize=12)
+plt.plot(inputs, firing_rates, color='black', lw=2)
+plt.ylim(*ylim_inset1)
 
-# plt.arrow(0.1, 0.45, 0, -0.15, color=colors['inh'])
-# plt.arrow(0.9, 0.45, 0, -0.15, color=colors['inh'])
-
-# plt.title('Before Learning')
-
-# plt.savefig('/Users/simonweber/doktor/TeX/learning_grids/inhibitory_plasticity/before_learning.pdf')
+# plt.savefig('/Users/simonweber/doktor/TeX/learning_grids/inhibitory_plasticity/inhibitory_plasticity.pdf')
 plt.show()
 
 ##################################
