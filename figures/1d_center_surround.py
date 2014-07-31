dcf1d0aa0756cf57b4b0c19d4dc9bd6b2cde18d2
Simon Weber <weber@tu-berlin.de>
2014-07-31 14:02:59 +0000

    Type commit message here…

--- figures/1d_center_surround.py
+++ figures/1d_center_surround.py
@@ -21,41 +21,80 @@ colors = {'exc': '#D7191C', 'inh': '#2C7BB6', 'diff': '0.4'}
 legend = {'exc': 'Excitation', 'inh': 'Inhibition', 'diff': 'Difference'}
 legend_short = {'exc': 'Exc.', 'inh': 'Inh.', 'diff': 'Difference'}
 signs = {'exc': 1, 'inh': -1}
-sigma = {'exc': 0.005, 'inh': 0.005}
+sigma = {'exc': 0.05, 'inh': 0.15}
 marker = {'exc': '^', 'inh': 'o'}
-# mpl.rcParams['legend.handlelength'] = 0
+populations = ['exc', 'inh']
+scaling_factor = {'exc': 1.0, 'inh': 0.5}
 
+# mpl.rcParams['legend.handlelength'] = 0
+current = {} 
+current['exc'] = np.array([0.1762616, 1.2, 0.8, -0.27795704, -1.46886165,
+	-0.8, -0.9, -0.50361893, -0.59372906, -0.33056701, 0.53445425, 
+	0.43721187]) + 1
+inputs = np.linspace(0., 1., len(current['exc']))
+
+
+fig = plt.figure(figsize=(0.8*4.7, 2.5))
 
 radius = 0.5
-x = np.linspace(-radius, radius, 200)
-plt.xlim([-radius, radius])
-plt.ylim([-4.5, 14])
+x = np.linspace(0, 1, 501)
+# plt.xlim([0.1, 0.8])
+plt.xlim([0.1, 0.9])
+# plt.ylim([-4.5, 14])
 
 c = 0.0
-sigma = {'exc': 0.03, 'inh': 0.1}
 gaussian = {}
 for t, s in sigma.iteritems():
 	gaussian[t] = scipy.stats.norm(loc=c, scale=s).pdf
 
-lw = {'exc': 2, 'inh': 2, 'diff': 4}
-plots = {'exc': gaussian['exc'](x), 'inh': gaussian['inh'](x), 'diff':gaussian['exc'](x) - gaussian['inh'](x)}
-for t, p in sorted(plots.iteritems(), reverse=True):
-	plt.plot(x, p, color=colors[t], lw=lw[t], label=legend[t])
+lw = {'exc': 2, 'inh': 2, 'diff': 2}
 
-plt.xlabel('position')
-plt.ylabel('firing rate')
-plt.legend(bbox_to_anchor=(1, 1), loc='upper right')
+for p in populations:
+	for n, i in enumerate(inputs):
+		gauss = scipy.stats.norm(loc=inputs[n], scale=sigma[p]).pdf
+		x = np.linspace(0, 1, 1001)
+		# Shift Gaussians slightyl (looks nicer)
+		alpha = 0.2
+		if n == 5:
+			alpha = 1.0
+			plt.plot(x,
+				(
+					scaling_factor['exc'] * np.sqrt(2*np.pi*sigma['exc']**2)
+					* scipy.stats.norm(loc=inputs[n], scale=sigma['exc']).pdf(x)
+					-
+					scaling_factor['inh'] * np.sqrt(2*np.pi*sigma['inh']**2)
+					* scipy.stats.norm(loc=inputs[n], scale=sigma['inh']).pdf(x)
+				),
+					color='black',
+					lw=lw['diff'])
+		sf = scaling_factor[p]
+		plt.plot(x, signs[p] * (sf * np.sqrt(2*np.pi*sigma[p]**2) * gauss(x)),
+					color=colors[p], alpha=alpha, lw=lw[p])
+
+
+
+# plt.xlabel('Stimulus')
+# plt.ylabel('Firing rate')
+# plt.legend(bbox_to_anchor=(1, 1), loc='upper right')
 
 ax = plt.gca()
-ax.get_xaxis().set_ticklabels([])
-ax.get_yaxis().set_ticklabels([0.0, 0])
+# ax.axis('off')
+plt.xticks([])
+plt.yticks([])
+# ax.get_xaxis().set_ticklabels([])
+# ax.get_yaxis().set_ticklabels([])
+
+# ax.get_yaxis().set_ticklabels([0.0, 0])
+
 # rectangle1=plt.Rectangle((-radius, 0),
 # 						radius, radius, ec='none', fc='r', lw=2)
-alpha = 0.3
-factor=2
-root = 0.04887
-ax.axvspan(-root, root, color=colors['exc'], alpha=alpha)
-ax.axvspan(-factor*sigma['inh'], -root, color=colors['inh'], alpha=alpha)
-ax.axvspan(root, factor*sigma['inh'], color=colors['inh'], alpha=alpha)
 
+# alpha = 0.3
+# factor=2
+# root = 0.04887
+# ax.axvspan(-root, root, color=colors['exc'], alpha=alpha)
+# ax.axvspan(-factor*sigma['inh'], -root, color=colors['inh'], alpha=alpha)
+# ax.axvspan(root, factor*sigma['inh'], color=colors['inh'], alpha=alpha)
+
+plt.savefig('/Users/simonweber/doktor/TeX/learning_grids/1dim_Gaussians/center_surround.pdf')
 plt.show()
---
+++ figures/input_tuning.py
@@ -0,0 +1,79 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats
+import os
+
+os.environ['PATH'] = os.environ['PATH'] + ':/usr/texbin'
+
+##############################################
+##########	General Plotting Stuff	##########
+##############################################
+mpl.rc('font', size=18)
+mpl.rc('legend', fontsize=18)
+mpl.rcParams.update({'figure.autolayout': True})
+
+# If you comment this out, then everything works, but in matplotlib fonts
+# mpl.rc('font', **{'family': 'serif', 'serif': ['Helvetica']})
+# mpl.rc('text', usetex=True)
+
+colors = {'exc': '#D7191C', 'inh': '#2C7BB6', 'diff': '0.4'}
+legend = {'exc': 'Excitation', 'inh': 'Inhibition', 'diff': 'Difference'}
+legend_short = {'exc': 'Exc.', 'inh': 'Inh.', 'diff': 'Difference'}
+signs = {'exc': 1, 'inh': -1}
+sigma = {'exc': 0.025, 'inh': 0.075}
+marker = {'exc': '^', 'inh': 'o'}
+populations = ['exc', 'inh']
+scaling_factor = {'exc': 1.0, 'inh': 0.5}
+
+
+fig = plt.figure(figsize=(3, 2))
+
+x = np.linspace(0, 1, 501)
+gaussian = {}
+c = 0.5
+for p, s in sigma.iteritems():
+	gaussian[p] = scipy.stats.norm(loc=c, scale=sigma[p]).pdf
+p = 'exc'
+plt.plot(x, np.sqrt(2*np.pi*sigma[p]**2) * gaussian[p](x), color=colors[p], lw=2)
+
+plt.ylim([-0.03, 1.03])
+plt.xlim([0.3, 0.7])
+plt.xticks([])
+plt.yticks([])
+plt.axis('off')
+
+ax = plt.gca()
+# Set y position for arrow to half the gaussian height
+# y_for_arrow = max(gaussian[p](x))/2
+y_for_arrow = 0.5
+# Draw an arrow between at height y_for_arrow and between mu-sigma and
+# mu+sigma
+# ax.annotate('', xy=(c-sigma[p], y_for_arrow),  xycoords='data',
+#                 xytext=(c+sigma[p], y_for_arrow), textcoords='data',
+#                 arrowprops=dict(arrowstyle='<->',
+#                 				lw=1,
+#                                 connectionstyle="arc3",
+#                                 shrinkA=0, shrinkB=0)
+#                 )
+
+
+arrowopts = {'shape': 'full', 'lw':1, 'length_includes_head':True,
+			'head_length':0.01, 'head_width':0.04, 'color':'black'}
+
+arrowlength = sigma[p] - 0.003
+plt.arrow(0.5, 0.5, arrowlength, 0, **arrowopts)
+plt.arrow(0.5, 0.5, -arrowlength, 0, **arrowopts)
+# Put the sigma underneath the arrow
+sigma_string = {'exc': r'$2 \sigma_{\mathrm{E}}$', 'inh': r'$2 \sigma_{\mathrm{I}}$'}
+ax.annotate(sigma_string[p], xy=(c, y_for_arrow-0.2), va='top', ha='center')
+
+# plt.autoscale(tight=True)
+# plt.tight_layout()
+name = 'input_tuning' + '_' + p + '_center_' + str(c).replace('.', 'p') + '.pdf'
+# plt.savefig('/Users/simonweber/doktor/TeX/learning_grids/input_tuning/' + name,
+# 	bbox_inches='tight', pad_inches=0.001)
+plt.savefig('/Users/simonweber/doktor/TeX/learning_grids/input_tuning/sigma_exc.pdf',
+	bbox_inches='tight', pad_inches=0.001)
+
+plt.show()
