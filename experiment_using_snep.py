1098f506665e9105ea2ccd895c7fb502996c2dc4
Simon Weber <weber@tu-berlin.de>
2014-09-11 15:21:20 +0000

    Simulation: 128 fields per synapse

--- experiment_using_snep.py
+++ experiment_using_snep.py
@@ -110,7 +110,7 @@ def main():
 
 	dimensions = 2
 	von_mises = False
-	fields_per_synapse = 32
+	fields_per_synapse = 128
 	fields_per_synapse_exc = fields_per_synapse
 	fields_per_synapse_inh = fields_per_synapse
 
