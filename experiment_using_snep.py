c03a178b3b1f1d0077925685d64f7c1faa44b1a3
Simon Weber <weber@tu-berlin.de>
2014-11-25 17:21:04 +0000

    Simulation: Band cells from more general inputs again

--- experiment_using_snep.py
+++ experiment_using_snep.py
@@ -46,7 +46,7 @@ def main():
 		boxtype = ['linear']
 		motion = 'persistent_semiperiodic'
 	else:
-		number_per_dimension = np.array([60, 60, 4])[:dimensions]
+		number_per_dimension = np.array([70, 70, 4])[:dimensions]
 		# boxtype = ['linear', 'circular']
 		boxtype = ['linear']
 		motion = 'persistent'
@@ -65,8 +65,8 @@ def main():
 	# n_inh = 1000
 	# radius = np.array([0.5, 1.0, 2.0, 3.0, 4.0])
 	radius = 0.5
-	eta_exc = 1e-6 / (2*radius)
-	eta_inh = 1e-5 / (2*radius)
+	eta_exc = 5e-6 / (2*radius)
+	eta_inh = 5e-5 / (2*radius)
 	# simulation_time = 8*radius*radius*10**5
 	# We want 100 fields on length 1
 	# length = 2*radius + 2*overlap
@@ -80,8 +80,8 @@ def main():
 						# [0.05, 0.2],
 						# [0.09, 0.15],
 						# [0.05, 0.7],
-						# [0.05, 0.07],
-						[0.07, 0.07],
+						[0.05, 0.07],
+						# [0.07, 0.07],
 						# [0.06],
 						# [0.03],
 						# [0.2, 0.4],
@@ -107,8 +107,8 @@ def main():
 						# [0.12, 1.5],
 						# [0.12, 0.6],
 						# [0.12, 0.6],
-						# [0.2, 0.04],
-						[1.5, 1.5],
+						[0.2, 0.04],
+						# [1.5, 1.5],
 						# [0.20],
 						# [0.38],
 						# [0.14, 0.7],
@@ -163,7 +163,7 @@ def main():
 			{
 			# 'sigma_noise':ParameterArray([0.1]),
 			# 'number_desired':ParameterArray(n),
-			'fields_per_synapse':ParameterArray([32]),
+			'fields_per_synapse':ParameterArray([8, 16, 32]),
 			# 'fields_per_synapse':ParameterArray([1, 2, 4, 8, 16, 32]),
 			# 'center_overlap':ParameterArray(center_overlap),
 			# 'sigma_x':ParameterArray([0.05, 0.1, 0.2]),
@@ -198,7 +198,7 @@ def main():
 			# 								),
 			'center_overlap':get_ParametersNamed(center_overlap_inh),
 			# 'number_desired':ParameterArray(n),
-			'fields_per_synapse':ParameterArray([32]),
+			'fields_per_synapse':ParameterArray([8, 16, 32]),
 			# 'fields_per_synapse':ParameterArray([1, 2, 4, 8, 16, 32]),
 			# 'center_overlap':ParameterArray(center_overlap),
 			# 'sigma_noise':ParameterArray([0.1]),
