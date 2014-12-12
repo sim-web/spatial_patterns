8fbc937e6d94d6f1095e8ac04392b25491277aeb
Simon Weber <weber@tu-berlin.de>
2014-12-12 11:43:18 +0000

    Simulation: nice grid video.

--- experiment_using_snep.py
+++ experiment_using_snep.py
@@ -31,7 +31,7 @@ from snep.configuration import config
 # config['multiproc'] = False
 config['network_type'] = 'empty'
 
-simulation_time = 1e4
+simulation_time = 1e7
 def main():
 	from snep.utils import Parameter, ParameterArray, ParametersNamed, flatten_params_to_point
 	from snep.experiment import Experiment
@@ -269,8 +269,8 @@ def main():
 			'boxtype': 'linear',
 			'radius': radius,
 			'diff_const': 0.01,
-			'every_nth_step': simulation_time/100,
-			'every_nth_step_weights': simulation_time/100,
+			'every_nth_step': simulation_time/1000,
+			'every_nth_step_weights': simulation_time/1000,
 			'seed_trajectory': 1,
 			'seed_init_weights': 1,
 			'seed_centers': 1,
