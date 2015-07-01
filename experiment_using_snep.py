80149546e44c0678338fb278683550778e09770c
Simon Weber <weber@tu-berlin.de>
2015-07-01 17:53:05 +0200

    Simulation: Grid spacing vs. inhibitory learning rate

--- experiment_using_snep.py
+++ experiment_using_snep.py
@@ -167,7 +167,7 @@ def main():
 			# 	[0.5, 1.0, 2.0, 4.0]),
 			# 'output_neurons':ParameterArray([3, 4]),
 			# 'seed_trajectory':ParameterArray([1, 2]),
-			# 'initial_x':ParameterArray([-radius/1.42, -radius/5.3, radius/1.08]),
+			'initial_x':ParameterArray([-radius/1.42, -radius/5.3, radius/1.08]),
 			# 'seed_init_weights':ParameterArray([1, 2]),
 			# 'lateral_inhibition':ParameterArray([False]),
 			# 'motion':ParameterArray(['persistent_semiperiodic', 'persistent_periodic', 'persistent']),
