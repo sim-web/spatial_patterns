a0c466c72d19b0789f6098a66c5ea01c01e99279
Simon Weber <weber@tu-berlin.de>
2015-07-03 11:45:38 +0200

    Simulation: fast grid establishment (stability check, by simulating 10 times longer)

--- experiment_using_snep.py
+++ experiment_using_snep.py
@@ -31,7 +31,7 @@ from snep.configuration import config
 # config['multiproc'] = False
 config['network_type'] = 'empty'
 
-simulation_time = 3e4
+simulation_time = 3e5
 def main():
 	from snep.utils import Parameter, ParameterArray, ParametersNamed, flatten_params_to_point
 	from snep.experiment import Experiment
