44bc122c7744f00c1934b96b20db27a265409691
Simon Weber <weber@tu-berlin.de>
2016-03-16 15:41:03 +0100

    Simulation: 500 minutes, place cell input, histogram 500 simulations

--- experiment_using_snep.py
+++ experiment_using_snep.py
@@ -142,7 +142,8 @@ class JobInfoExperiment(Experiment):
 
 		# Note: 15.5e4 steps correspond to 50 minutes in
 		# sargolini trajectory
-		simulation_time = 15.5e4
+		time_factor = 10
+		simulation_time = 15.5e4 * time_factor
 		every_nth_step = simulation_time / 100
 		np.random.seed(1)
 		n_simulations = 500
@@ -172,8 +173,8 @@ class JobInfoExperiment(Experiment):
 		target_rate = 1.0
 		# radius = np.array([0.5, 1.0, 2.0, 3.0, 4.0])
 		radius = 0.5
-		eta_inh = 16e-3 / (2*radius) / 20.
-		eta_exc = 40e-4 / (2*radius) / 20.
+		eta_inh = 16e-3 / (2*radius) / 20. / time_factor
+		eta_exc = 40e-4 / (2*radius) / 20. / time_factor
 
 		sigma_exc = np.array([
 			[0.05, 0.05],
