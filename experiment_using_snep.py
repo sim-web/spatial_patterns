7d1aa1d11b1ef12d9d5a75a6838d167b391e15c9
Simon Weber <weber@tu-berlin.de>
2016-05-03 16:26:13 +0200

    Simulation: 2D GRF, fixed_mean vs stretch, larger init weight

--- experiment_using_snep.py
+++ experiment_using_snep.py
@@ -218,8 +218,8 @@ class JobInfoExperiment(Experiment):
 
 		gaussian_process = True
 		if gaussian_process:
-			init_weight_exc = 0.1
-			# init_weight_exc = 1.0
+			# init_weight_exc = 0.1
+			init_weight_exc = 1.0
 			symmetric_centers = False
 		else:
 			init_weight_exc = 1.0
