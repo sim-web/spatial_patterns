2474122e8dd81c600cffb4ae501d089b2f06dde6
Simon Weber <weber@tu-berlin.de>
2016-05-09 11:52:52 +0200

    Simulation: 2D GRF, sigma_inh 0.1, histogram, reduced eta

--- experiment_using_snep.py
+++ experiment_using_snep.py
@@ -160,8 +160,8 @@ class JobInfoExperiment(Experiment):
 			number_per_dimension_inh = np.array([3, 3])
 
 
-		every_nth_step = simulation_time / 8
-		every_nth_step_weights = simulation_time / 8
+		every_nth_step = simulation_time / 100
+		every_nth_step_weights = simulation_time / 100
 		random_sample_x = np.random.random_sample(n_simulations)
 		random_sample_y = np.random.random_sample(n_simulations)
 
@@ -185,8 +185,8 @@ class JobInfoExperiment(Experiment):
 
 		target_rate = 1.0
 		radius = 0.5
-		eta_inh = 10. * 3e-4 / (2*radius * 10. * 5.5)
-		eta_exc = 10. * 3e-5 / (2*radius * 10. * 22)
+		eta_inh = 5. * 3e-4 / (2*radius * 10. * 5.5)
+		eta_exc = 5. * 3e-5 / (2*radius * 10. * 22)
 
 		# sinh = np.arange(0.08, 0.36, 0.02)
 		# sexc = np.tile(0.03, len(sinh))
@@ -242,7 +242,7 @@ class JobInfoExperiment(Experiment):
 		# Interesting seed selection for 500 fps, learning rate 0.003
 		# seed_centers = np.array([12, 47, 93, 104, 142, 203, 228, 267])
 		# Interesting seed selection for GRF, sigma_inh 0.1
-		seed_centers = np.array([0, 2, 3, 27, 83, 320, 385, 413])
+		# seed_centers = np.array([0, 2, 3, 27, 83, 320, 385, 413])
 
 		# For string arrays you need the list to start with the longest string
 		# you can automatically achieve this using .sort(key=len, reverse=True)
@@ -303,10 +303,10 @@ class JobInfoExperiment(Experiment):
 		}
 
 		params = {
-			'visual': 'figure',
-			# 'visual': 'none',
-			# 'to_clear': 'weights_output_rate_grid_gp_extrema_centers',
-			'to_clear': 'none',
+			# 'visual': 'figure',
+			'visual': 'none',
+			'to_clear': 'weights_output_rate_grid_gp_extrema_centers',
+			# 'to_clear': 'none',
 			'sim':
 				{
 					'input_normalization': 'figure',
