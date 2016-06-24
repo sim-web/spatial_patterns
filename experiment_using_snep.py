3cecdb0f188d00225446bff1ef55357db61e2dad
Simon Weber <weber@tu-berlin.de>
2016-06-24 17:30:29 +0200

    Simulation: test, conjunctive cell, 20 fields per synapse

--- experiment_using_snep.py
+++ experiment_using_snep.py
@@ -207,8 +207,8 @@ class JobInfoExperiment(Experiment):
 
 		target_rate = 1.0
 		radius = 0.5
-		eta_exc = 0.5e-5 / (2*radius)
-		eta_inh = 0.5e-4 / (2*radius)
+		eta_exc = 0.4e-5 / (2*radius)
+		eta_inh = 0.4e-4 / (2*radius)
 		# eta_exc = 40 * 1e-5 / (2*radius)
 		# eta_inh = 40 * 1e-4 / (2*radius)
 
