9eb5eccd1678c3ee4d7a67e38b3779a826e9940b
Simon Weber <weber@tu-berlin.de>
2016-12-10 12:48:00 +0100

    Changes the add computed function for histogram. Now lists and not tuples are used as arguments. Tuples do not work if they have a single entry.

--- add_computed.py
+++ add_computed.py
@@ -150,10 +150,10 @@ class Add_computed(plotting.Plot):
 										 overwrite=self.overwrite)
 
 	def grid_scores_for_all_times_and_seeds(self,
-											methods=('Weber', 'sargolini',
-													 'sargolini_extended'),
-											n_cumulatives=(1, 3),
-											types=('hexagonal', 'quadratic')):
+											methods=['Weber', 'sargolini',
+													 'sargolini_extended'],
+											n_cumulatives=[1, 3],
+											types=['hexagonal', 'quadratic']):
 		"""
 		Adds and array of grid scores for all times and seeds
 
@@ -162,9 +162,9 @@ class Add_computed(plotting.Plot):
 
 		Parameters
 		----------
-		methods : tuple
-		n_cumulatives : tuple
-		types : tuple
+		methods : list
+		n_cumulatives : list
+		types : list
 
 
 		Returns
@@ -395,11 +395,7 @@ if __name__ == '__main__':
 	# date_dir = '2015-09-14-16h03m44s'
 	# date_dir = '2016-04-19-11h41m44s_20_fps'
 	# date_dir = '2016-04-20-15h11m05s_20_fps_learning_rate_0.2'
-	for date_dir in ['2016-12-07-17h14m12s',
-					 '2016-12-07-17h18m52s',
-					 '2016-12-07-17h21m04s',
-					 '2016-12-07-17h23m22s',
-					 '2016-12-07-17h29m12s']:
+	for date_dir in ['2016-12-08-13h54m37s_500_simulations_500_fps_more_neurons']:
 		tables = snep.utils.make_tables_from_path(
 			general_utils.snep_plotting.get_path_to_hdf_file(date_dir))
 
@@ -414,7 +410,8 @@ if __name__ == '__main__':
 		# add_computed.grid_score_2d(type='hexagonal')
 		# add_computed.grid_score_2d(type='quadratic')
 		# add_computed.mean_inter_peak_distance()
-		add_computed.grid_scores_for_all_times_and_seeds()
+		add_computed.grid_scores_for_all_times_and_seeds(methods=['sargolini'],
+														 types=['hexagonal'])
 		add_computed.grid_angles_for_all_times_and_seeds(minimum_grid_score=0.7)
 		# add_computed.parameter_string_for_table()
 		# add_computed.mean_correlogram()
