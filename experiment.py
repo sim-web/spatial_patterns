f346577a5d8d9b0eb5eb70d57a3783a91575d1b8
Simon Weber <weber@tu-berlin.de>
2018-02-12 15:41:14 +0100

    Cleaned up and added comments.

--- experiment.py
+++ experiment.py
@@ -9,53 +9,58 @@ from spatial_patterns import parameters
 ##############################################
 ### One dimension ###
 # 1 dimension, place cells to grid cell
-# params = parameters.params_1d_place2grid
+params = parameters.params_1d_place2grid
 # # 1 dimension, non-localized input to grid cell
 # params = parameters.params_1d_non_localized2grid
 # # 1 dimension, non-localized input to place cell
 # params = parameters.params_1d_non_localized2place
 # # 1 dimension, non-localized input to invariant pattern
-# params = parameters.params_1d_non_localized2invarant
+# params = parameters.params_1d_non_localized2invariant
+
 ### Two dimensions ###
+# 2 dimensions, place cells to grid cell
 # params = parameters.params_2d_place2grid
-# params = parameters.params_2d_place2grid_fast
-### Fast 1D Test ###
+# 2 dimensions, non-localized to grid cell
+# params = parameters.params_2d_non_localized2grid
+
+### Fast 1D Test ##s#
 # params = parameters.params_test
 ### Fast 2D Test ###
-params = parameters.params_test_2d
+# params = parameters.params_test_2d
 
 ###########################################################################
 ############################## Run the code ###############################
 ###########################################################################
-# The code should return all the rawdata as a nested dictionary whose
-# final leaves are arrays
-# See initialization.py for the run function
+# Instantiate a rat
 rat = Rat(params)
+# Let the rat run and learn
 rawdata = rat.run()
-# rawdata is a dictionary of dictionaries (arbitrarily nested) with
-# keys (strings) and values (arrays or deeper dictionaries)
-# snep creates a group for each dictionary key and finally an array for
-# the deepest value. you can do this for raw_data or computed
-# whenever you wish
 
+###############################################
+################ Plot the data ################
+###############################################
+# Instantiate the Plot class.
+# You can select any plotting function from the Plot class
 plot = plotting.Plot(params=params, rawdata=rawdata)
-# plt.subplot(211)
-# plot.plot_output_rates_from_equation(time=0)
-# plt.subplot(212)
-# plot.plot_output_rates_from_equation(time=params['sim']['simulation_time'])
-# fig = plt.figure(figsize=(5,5))
-if params['sim']['dimensions'] == 1:
+
+dim = params['sim']['dimensions']
+# In one dimension, show a heat map of the time evolution
+if dim == 1:
     plot.output_rate_heat_map(start_time=0,
                               end_time=params['sim']['simulation_time'],
                               from_file=True, maximal_rate=7)
     plt.colorbar()
-
-elif params['sim']['dimensions'] == 2:
+# In two dimensions, show 4 example screenshots
+elif dim == 2:
     t_sim = params['sim']['simulation_time']
     gs = gridspec.GridSpec(1, 3)
     for n, time in enumerate([0, t_sim/2, t_sim]):
         plt.subplot(gs[n])
         plot.plot_output_rates_from_equation(time=time, from_file=True)
+        # Plot the correlogram instead
+        # plot.plot_correlogram(time=time, from_file=True)
 
+# Show the plot. NB: You might need a different backend.
 plt.show()
+# Save the figure to disk
 # plt.savefig(path='figure.png', dpi=300)
