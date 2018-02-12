0fa1af3d304da65847c3aabb3dc106cd6469c916
Simon Weber <weber@tu-berlin.de>
2018-02-12 18:02:38 +0100

    Fixed figure size.

--- experiment.py
+++ experiment.py
@@ -46,12 +46,14 @@ plot = plotting.Plot(params=params, rawdata=rawdata)
 dim = params['sim']['dimensions']
 # In one dimension, show a heat map of the time evolution
 if dim == 1:
+    fig = plt.figure(figsize=(7, 3))
     plot.output_rate_heat_map(start_time=0,
                               end_time=params['sim']['simulation_time'],
                               from_file=True, maximal_rate=7)
     plt.colorbar()
 # In two dimensions, show 4 example screenshots
 elif dim == 2:
+    plt.figure(figsize=(6, 1.5))
     t_sim = params['sim']['simulation_time']
     gs = gridspec.GridSpec(1, 3)
     for n, time in enumerate([0, t_sim/2, t_sim]):
